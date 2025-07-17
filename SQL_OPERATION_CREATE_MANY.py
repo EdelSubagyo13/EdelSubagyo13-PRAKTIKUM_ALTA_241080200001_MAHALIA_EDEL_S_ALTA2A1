0e01f51ad3d2a682432393aee75058885bbc2e05
EdelSubagyo13 <edelsubagyo11@gmail.com>
2025-07-17 23:20:45 +0700

    Add files via upload
    PBL02

---
+++ SQL_OPERATION_CREATE_MANY.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+koneksi_ke_DB = sqlite3.connect("cars.db")
+
+k = koneksi_ke_DB.cursor()
+
+k.execute("""
+INSERT INTO TBCars (
+    id,
+    name,
+    brand,
+    model,
+    price
+) VALUES (
+    2,
+    'Black Car',
+    'BMW',
+    'M3',
+    12000
+)
+""")
+
+koneksi_ke_DB.commit()
+koneksi_ke_DB.close()
---
+++ SQL_OPERATION_SEARCH.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+koneksi_ke_DB = sqlite3.connect("cars.db")
+
+k = koneksi_ke_DB.cursor()
+
+k.execute("""
+    SELECT * FROM TBCars
+    WHERE
+        id = 2
+""")
+
+print(k.fetchall())
+
+koneksi_ke_DB.close()
